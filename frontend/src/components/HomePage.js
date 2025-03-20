4d59528dbf36c21798ab6c6394c77bbeaad2f5b5
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-03-20 11:41:57 -0400

    Update HomePage.js

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,10 +1,8 @@
 import AnalyzeDataButton from "./AnalyzeDataButton";
 import FitbitLoginForm from "./FitbitLoginForm";
 import FitbitSyncButton from "./FitbitSyncButton";
-import { useAuth } from "../context/AuthContext"
 
 function HomePage() {
-  const { isLoggedIn } = useAuth();
   return (
     <div className="HomePage">
       <h2>Welcome</h2>
